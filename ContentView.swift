476a7add79a4e47645e66a32f2bd6883f6c61d21
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2023-06-28 23:49:58 +0200

    Add files via upload

--- ContentView.swift
+++ ContentView.swift
@@ -18,14 +18,17 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            Color.blue
+            //Color.blue
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             VStack(spacing: 30) {
                 VStack {
                     Text("tap the correct flag")
                         .foregroundColor(.white)
+                        .font(.subheadline.weight(.heavy))
                     Text(countries[correctAnswer])
                         .foregroundColor(.white)
+                        .font(.largeTitle.weight(.semibold))
                 }
                 ForEach(0..<3) { number in
                     Button {
@@ -34,6 +37,8 @@ struct ContentView: View {
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .shadow(radius: 5)
                     }
                 }
             }
